ad862aaace980e9be0897b48a821b9f3439b9e56
Burchard36 <burchard36@gmail.com>
2024-02-02 13:30:36 -0800

    If this works i would actually be surprised (Myrmex Crash fix for TFC)

--- src/main/java/com/allthemods/gravitas2/core/mixin/WorldGenCyclopsCaveMixin.java
+++ src/main/java/com/allthemods/gravitas2/core/mixin/WorldGenCyclopsCaveMixin.java
@@ -1,6 +1,12 @@
 package com.allthemods.gravitas2.core.mixin;
 
 import com.allthemods.gravitas2.GregitasCore;
+import com.github.alexthe666.iceandfire.IafConfig;
+import com.github.alexthe666.iceandfire.block.BlockGoldPile;
+import com.github.alexthe666.iceandfire.block.IafBlockRegistry;
+import com.github.alexthe666.iceandfire.entity.EntityCyclops;
+import com.github.alexthe666.iceandfire.entity.IafEntityRegistry;
+import com.github.alexthe666.iceandfire.util.WorldUtil;
 import com.github.alexthe666.iceandfire.world.gen.TypedFeature;
 import com.github.alexthe666.iceandfire.world.gen.WorldGenCyclopsCave;
 import com.mojang.serialization.Codec;
@@ -9,52 +15,153 @@ import net.dries007.tfc.common.entities.TFCEntities;
 import net.dries007.tfc.common.entities.livestock.WoolyAnimal;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.block.AbstractChestBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.ChestBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Mixin(value = WorldGenCyclopsCave.class, remap = false)
 public abstract class WorldGenCyclopsCaveMixin extends Feature<NoneFeatureConfiguration> implements TypedFeature {
 
+
+    @Shadow
+    public static final ResourceLocation CYCLOPS_CHEST = new ResourceLocation("iceandfire", "chest/cyclops_cave");
+    @Shadow
+    private static Direction[] HORIZONTALS;
+
     public WorldGenCyclopsCaveMixin(Codec<NoneFeatureConfiguration> codec) {
         super(codec);
     }
-    /**
-     * @author thevortex
-     * @reason placeholder
-     */
+
     @Overwrite
-    private static void generateShell(final FeaturePlaceContext<NoneFeatureConfiguration> context, int size) {
+    public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
+        BlockPos anActualUsableLocation = context.origin().offset(0, Math.abs(context.origin().getY()) + context.random().nextInt(36), 0);
+
+        if (!WorldUtil.canGenerate(IafConfig.spawnCyclopsCaveChance, context.level(), context.random(), anActualUsableLocation, this.getId(), true)) {
+            GregitasCore.LOGGER.info("Failed to generate a cyclops cave at: " + anActualUsableLocation);
+            return false;
+        } else {
+            GregitasCore.LOGGER.info("Attempting to generate a cyclops cave @ ... %s " + anActualUsableLocation);
+            int size = 16;
+            int distance = 6;
+            //if (!context.level().isEmptyBlock(context.origin().offset(size - distance, -3, -size + distance)) || !context.level().isEmptyBlock(context.origin().offset(size - distance, -3, size - distance)) || !context.level().isEmptyBlock(context.origin().offset(-size + distance, -3, -size + distance)) || !context.level().isEmptyBlock(context.origin().offset(-size + distance, -3, size - distance))) {
+                generateShell(context, size, anActualUsableLocation);
+                int innerSize = size - 2;
+                int x = innerSize + context.random().nextInt(2);
+                int y = 10 + context.random().nextInt(2);
+                int z = innerSize + context.random().nextInt(2);
+                float radius = (float)(x + y + z) * 0.333F + 0.5F;
+                int sheepPenCount = 0;
+                Iterator var10 = ((Set)BlockPos.betweenClosedStream(anActualUsableLocation.offset(-x, -y, -z), anActualUsableLocation.offset(x, y, z)).map(BlockPos::immutable).collect(Collectors.toSet())).iterator();
+
+                BlockPos position;
+                while(var10.hasNext()) {
+                    position = (BlockPos)var10.next();
+                    if (position.distSqr(anActualUsableLocation) <= (double)(radius * radius) && position.getY() > anActualUsableLocation.getY() && !(context.level().getBlockState(anActualUsableLocation).getBlock() instanceof AbstractChestBlock)) {
+                        context.level().setBlock(position, Blocks.AIR.defaultBlockState(), 3);
+                    }
+                }
+
+                var10 = ((Set)BlockPos.betweenClosedStream(anActualUsableLocation.offset(-x, -y, -z), anActualUsableLocation.offset(x, y, z)).map(BlockPos::immutable).collect(Collectors.toSet())).iterator();
+
+                while(true) {
+                    do {
+                        do {
+                            do {
+                                do {
+                                    if (!var10.hasNext()) {
+                                        EntityCyclops cyclops = (EntityCyclops)((EntityType) IafEntityRegistry.CYCLOPS.get()).create(context.level().getLevel());
+                                        cyclops.absMoveTo((double)anActualUsableLocation.getX() + 0.5, (double)anActualUsableLocation.getY() + 1.5, (double)anActualUsableLocation.getZ() + 0.5, context.random().nextFloat() * 360.0F, 0.0F);
+                                        context.level().addFreshEntity(cyclops);
+                                        return true;
+                                    }
+
+                                    position = (BlockPos)var10.next();
+                                } while(!(position.distSqr(anActualUsableLocation) <= (double)(radius * radius)));
+                            } while(position.getY() != anActualUsableLocation.getY());
+
+                            if (context.random().nextInt(130) == 0 && this.isTouchingAir(context.level(), position.above())) {
+                                this.generateSkeleton(context.level(), position.above(), context.random(), anActualUsableLocation, radius);
+                            }
+
+                            if (context.random().nextInt(130) == 0 && position.distSqr(anActualUsableLocation) <= (double)(radius * radius) * 0.800000011920929 && sheepPenCount < 2) {
+                                this.generateSheepPen(context.level(), position.above(), context.random(), anActualUsableLocation, radius);
+                                ++sheepPenCount;
+                            }
+
+                            if (context.random().nextInt(80) == 0 && this.isTouchingAir(context.level(), position.above())) {
+                                context.level().setBlock(position.above(), IafBlockRegistry.GOLD_PILE.get().defaultBlockState().setValue(BlockGoldPile.LAYERS, 8), 3);
+                                context.level().setBlock(position.above().north(), IafBlockRegistry.GOLD_PILE.get().defaultBlockState().setValue(BlockGoldPile.LAYERS, 1 + (new Random()).nextInt(7)), 3);
+                                context.level().setBlock(position.above().south(), IafBlockRegistry.GOLD_PILE.get().defaultBlockState().setValue(BlockGoldPile.LAYERS, 1 + (new Random()).nextInt(7)), 3);
+                                context.level().setBlock(position.above().west(), IafBlockRegistry.GOLD_PILE.get().defaultBlockState().setValue(BlockGoldPile.LAYERS, 1 + (new Random()).nextInt(7)), 3);
+                                context.level().setBlock(position.above().east(), IafBlockRegistry.GOLD_PILE.get().defaultBlockState().setValue(BlockGoldPile.LAYERS, 1 + (new Random()).nextInt(7)), 3);
+                                context.level().setBlock(position.above(2), Blocks.CHEST.defaultBlockState().setValue(ChestBlock.FACING, HORIZONTALS[(new Random()).nextInt(3)]), 2);
+                                if (context.level().getBlockState(position.above(2)).getBlock() instanceof AbstractChestBlock) {
+                                    BlockEntity blockEntity = context.level().getBlockEntity(position.above(2));
+                                    if (blockEntity instanceof ChestBlockEntity) {
+                                        ChestBlockEntity chestBlockEntity = (ChestBlockEntity)blockEntity;
+                                        chestBlockEntity.setLootTable(CYCLOPS_CHEST, context.random().nextLong());
+                                    }
+                                }
+                            }
+                        } while(context.random().nextInt(50) != 0);
+                    } while(!this.isTouchingAir(context.level(), position.above()));
+
+                    int torchHeight = context.random().nextInt(2) + 1;
+
+                    for(int fence = 0; fence < torchHeight; ++fence) {
+                        context.level().setBlock(position.above(1 + fence), this.getFenceState(context.level(), position.above(1 + fence)), 3);
+                    }
+
+                    context.level().setBlock(position.above(1 + torchHeight), Blocks.TORCH.defaultBlockState(), 2);
+                }
+            //} else {
+            //    GregitasCore.LOGGER.info("ABSOLUTELY FAILED to generate a cyclops cave");
+            //    return false;
+            //}
+        }
+    }
+
+    private static void generateShell(final FeaturePlaceContext<NoneFeatureConfiguration> context, int size, BlockPos loc) {
         int x = size + context.random().nextInt(2);
         int y = 12 + context.random().nextInt(2);
         int z = size + context.random().nextInt(2);
         float radius = (x + y + z) * 0.333F + 0.5F;
 
-        for (BlockPos position : BlockPos.betweenClosedStream(context.origin().offset(-x, -y, -z), context.origin().offset(x, y, z)).map(BlockPos::immutable).collect(Collectors.toSet())) {
-            boolean doorwayX = position.getX() >= context.origin().getX() - 2 + context.random().nextInt(2) && position.getX() <= context.origin().getX() + 2 + context.random().nextInt(2);
-            boolean doorwayZ = position.getZ() >= context.origin().getZ() - 2 + context.random().nextInt(2) && position.getZ() <= context.origin().getZ() + 2 + context.random().nextInt(2);
-            boolean isNotInDoorway = !doorwayX && !doorwayZ && position.getY() > context.origin().getY() || position.getY() > context.origin().getY() + y - (3 + context.random().nextInt(2));
+        for (BlockPos position : BlockPos.betweenClosedStream(loc.offset(-x, -y, -z), loc.offset(x, y, z)).map(BlockPos::immutable).collect(Collectors.toSet())) {
+            boolean doorwayX = position.getX() >= loc.getX() - 2 + context.random().nextInt(2) && position.getX() <= loc.getX() + 2 + context.random().nextInt(2);
+            boolean doorwayZ = position.getZ() >= loc.getZ() - 2 + context.random().nextInt(2) && position.getZ() <= loc.getZ() + 2 + context.random().nextInt(2);
+            boolean isNotInDoorway = !doorwayX && !doorwayZ && position.getY() > loc.getY() || position.getY() > loc.getY() + y - (3 + context.random().nextInt(2));
 
-            if (position.distSqr(context.origin()) <= radius * radius) {
+            if (position.distSqr(loc) <= radius * radius) {
                 BlockState state = context.level().getBlockState(position);
 
                 if (!(state.getBlock() instanceof AbstractChestBlock) && state.getDestroySpeed(context.level(), position) >= 0 && isNotInDoorway) {
                     context.level().setBlock(position, TFCBlocks.PLAIN_ALABASTER.get().defaultBlockState(), Block.UPDATE_ALL);
                 }
-                if (position.getY() == context.origin().getY()) {
+                if (position.getY() == loc.getY()) {
                     context.level().setBlock(position, TFCBlocks.PLAIN_ALABASTER_BRICKS.get().defaultBlockState(), Block.UPDATE_ALL);
                 }
-                if (position.getY() <= context.origin().getY() - 1 && !state.canOcclude()) {
+                if (position.getY() <= loc.getY() - 1 && !state.canOcclude()) {
                     context.level().setBlock(position, TFCBlocks.PLAIN_ALABASTER_BRICKS.get().defaultBlockState(), Block.UPDATE_ALL);
                 }
             }
@@ -118,6 +225,13 @@ public abstract class WorldGenCyclopsCaveMixin extends Feature<NoneFeatureConfig
         Block calcite = TFCBlocks.CALCITE.get();
         return calcite.defaultBlockState();
     }
+
+    @Shadow
+    abstract void generateSkeleton(LevelAccessor level, BlockPos position, RandomSource random, BlockPos origin, float radius);
+
+    @Shadow
+    abstract boolean isTouchingAir(LevelAccessor level, BlockPos position);
+
 }
 
 
--- src/main/java/com/allthemods/gravitas2/core/mixin/WorldGenMyrmexHiveMixin.java
+++ src/main/java/com/allthemods/gravitas2/core/mixin/WorldGenMyrmexHiveMixin.java
@@ -1,6 +1,8 @@
 package com.allthemods.gravitas2.core.mixin;
 
 import com.github.alexthe666.iceandfire.IafConfig;
+import com.github.alexthe666.iceandfire.entity.*;
+import com.github.alexthe666.iceandfire.entity.util.MyrmexHive;
 import com.github.alexthe666.iceandfire.world.IafWorldRegistry;
 import com.github.alexthe666.iceandfire.world.MyrmexWorldData;
 import com.github.alexthe666.iceandfire.world.gen.TypedFeature;
@@ -8,11 +10,16 @@ import com.github.alexthe666.iceandfire.world.gen.WorldGenMyrmexHive;
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
@@ -35,6 +42,19 @@ public abstract class WorldGenMyrmexHiveMixin extends Feature<NoneFeatureConfigu
     private int entrances = 0;
     @Shadow
     private BlockPos centerOfHive;
+    @Shadow
+    public MyrmexHive hive;
+    @Shadow
+    private boolean jungle;
+
+    @Shadow
+    private static BlockState DESERT_RESIN;
+    @Shadow
+    private static BlockState STICKY_DESERT_RESIN;
+    @Shadow
+    private static BlockState JUNGLE_RESIN;
+    @Shadow
+    private static BlockState STICKY_JUNGLE_RESIN;
 
     public WorldGenMyrmexHiveMixin(Codec<NoneFeatureConfiguration> codex) {
         super(codex);
@@ -44,30 +64,91 @@ public abstract class WorldGenMyrmexHiveMixin extends Feature<NoneFeatureConfigu
     @Overwrite
     private void decorateCircle(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, Direction direction) {
         int radius = size + 2;
-        {
-            for (float i = 0; i < radius; i += 0.5) {
-                for (float j = 0; j < 2 * Math.PI * i; j += 0.5) {
-                    int x = (int) Math.floor(Mth.sin(j) * i);
-                    int z = (int) Math.floor(Mth.cos(j) * i);
-                    try {
-                        if (direction == Direction.WEST || direction == Direction.EAST) {
-                            if (world.isEmptyBlock(position.offset(0, x, z))) {
-                                decorate(world, position.offset(0, x, z), position, size, rand, WorldGenMyrmexHive.RoomType.TUNNEL);
-                            }
-                        } else {
-                            if (world.isEmptyBlock(position.offset(x, z, 0))) {
-                                decorate(world, position.offset(x, z, 0), position, size, rand, WorldGenMyrmexHive.RoomType.TUNNEL);
-                            }
-                        }
-                        if (world.isEmptyBlock(position.offset(0, x, (z - 1)))) {
-                            decorateTubers(world, position.offset(0, x, z), rand, WorldGenMyrmexHive.RoomType.TUNNEL);
-                        }
-                    } catch (Exception ex) { // Bad practive but im even worse at math so bite me
-                        ex.printStackTrace();
+
+        for (float i = 0; i < radius; i += 0.5) {
+            for (float j = 0; j < 2 * Math.PI * i; j += 0.5) {
+                int x = (int) Math.floor(Mth.sin(j) * i);
+                int z = (int) Math.floor(Mth.cos(j) * i);
+
+                try {
+                    BlockPos offsetPos;
+
+                    if (direction == Direction.WEST || direction == Direction.EAST) {
+                        offsetPos = position.offset(0, x, z);
+                    } else {
+                        offsetPos = position.offset(x, z, 0);
                     }
+
+                    if (world.isAreaLoaded(offsetPos, 1)) {
+                        if (world.isEmptyBlock(offsetPos)) {
+                            decorate(world, offsetPos, position, size, rand, WorldGenMyrmexHive.RoomType.TUNNEL);
+                        }
+                    }
+
+                    BlockPos tuberPos = position.offset(0, x, (z - 1));
+
+                    if (world.isAreaLoaded(tuberPos, 1) && world.isEmptyBlock(tuberPos)) {
+                        decorateTubers(world, tuberPos, rand, WorldGenMyrmexHive.RoomType.TUNNEL);
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
             }
         }
+    }
+
+
+    @Overwrite
+    private void generateMainRoom(ServerLevelAccessor world, RandomSource rand, BlockPos position) {
+        this.hive = new MyrmexHive(world.getLevel(), position, 100);
+        MyrmexWorldData.addHive(world.getLevel(), this.hive);
+        BlockState resin = this.jungle ? JUNGLE_RESIN : DESERT_RESIN;
+        BlockState sticky_resin = this.jungle ? STICKY_JUNGLE_RESIN : STICKY_DESERT_RESIN;
+        this.generateSphere(world, rand, position, 14, 7, resin, sticky_resin);
+        this.generateSphere(world, rand, position, 12, 5, Blocks.AIR.defaultBlockState());
+        this.decorateSphere(world, rand, position, 12, 5, WorldGenMyrmexHive.RoomType.QUEEN);
+        this.generatePath(world, rand, position.relative(Direction.NORTH, 7).below(), 10 + rand.nextInt(8), Direction.NORTH, 100);
+        this.generatePath(world, rand, position.relative(Direction.SOUTH, 7).below(), 10 + rand.nextInt(8), Direction.SOUTH, 100);
+        this.generatePath(world, rand, position.relative(Direction.WEST, 7).below(), 10 + rand.nextInt(8), Direction.WEST, 100);
+        this.generatePath(world, rand, position.relative(Direction.EAST, 7).below(), 10 + rand.nextInt(8), Direction.EAST, 100);
+        if (!this.small) {
+            EntityMyrmexQueen queen = new EntityMyrmexQueen(IafEntityRegistry.MYRMEX_QUEEN.get(), world.getLevel());
+            BlockPos ground = MyrmexHive.getGroundedPos(world, position);
+            queen.finalizeSpawn(world, world.getCurrentDifficultyAt(ground), MobSpawnType.CHUNK_GENERATION, null, null);
+            queen.setHive(this.hive);
+            queen.setJungleVariant(this.jungle);
+            queen.absMoveTo((double)ground.getX() + 0.5, (double)ground.getY() + 1.0, (double)ground.getZ() + 0.5, 0.0F, 0.0F);
+            world.addFreshEntity(queen);
+
+            int i;
+            for(i = 0; i < 4 + rand.nextInt(3); ++i) {
+                EntityMyrmexBase myrmex = new EntityMyrmexWorker(IafEntityRegistry.MYRMEX_WORKER.get(), world.getLevel());
+                myrmex.finalizeSpawn(world, world.getCurrentDifficultyAt(ground), MobSpawnType.CHUNK_GENERATION, null, null);
+                myrmex.setHive(this.hive);
+                myrmex.absMoveTo((double)ground.getX() + 0.5, (double)ground.getY() + 1.0, (double)ground.getZ() + 0.5, 0.0F, 0.0F);
+                myrmex.setJungleVariant(this.jungle);
+                world.addFreshEntity(myrmex);
+            }
+
+            for(i = 0; i < 2 + rand.nextInt(2); ++i) {
+                EntityMyrmexBase myrmex = new EntityMyrmexSoldier(IafEntityRegistry.MYRMEX_SOLDIER.get(), world.getLevel());
+                myrmex.finalizeSpawn(world, world.getCurrentDifficultyAt(ground), MobSpawnType.CHUNK_GENERATION, null, null);
+                myrmex.setHive(this.hive);
+                myrmex.absMoveTo((double)ground.getX() + 0.5, (double)ground.getY() + 1.0, (double)ground.getZ() + 0.5, 0.0F, 0.0F);
+                myrmex.setJungleVariant(this.jungle);
+                world.addFreshEntity(myrmex);
+            }
+
+            for(i = 0; i < rand.nextInt(2); ++i) {
+                EntityMyrmexBase myrmex = new EntityMyrmexSentinel(IafEntityRegistry.MYRMEX_SENTINEL.get(), world.getLevel());
+                myrmex.finalizeSpawn(world, world.getCurrentDifficultyAt(ground), MobSpawnType.CHUNK_GENERATION, null, null);
+                myrmex.setHive(this.hive);
+                myrmex.absMoveTo((double)ground.getX() + 0.5, (double)ground.getY() + 1.0, (double)ground.getZ() + 0.5, 0.0F, 0.0F);
+                myrmex.setJungleVariant(this.jungle);
+                world.addFreshEntity(myrmex);
+            }
+        }
+
     }
 
     @Overwrite
@@ -101,7 +182,56 @@ public abstract class WorldGenMyrmexHiveMixin extends Feature<NoneFeatureConfigu
             this.small = false;
             return true;
         }
+        //return false;
     }
+
+    @Overwrite
+    private void generateEntrance(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, Direction direction) {
+        BlockPos up = position.above();
+        this.hive.getEntranceBottoms().put(up, direction);
+
+        while(up.getY() < world.getHeightmapPos(this.small ? Heightmap.Types.MOTION_BLOCKING_NO_LEAVES : Heightmap.Types.WORLD_SURFACE_WG, up).getY() && !world.getBlockState(up).is(BlockTags.LOGS)) {
+            this.generateCircleRespectSky(world, rand, up, size, height, direction);
+            up = up.above().relative(direction);
+        }
+
+        BlockState resin = this.jungle ? JUNGLE_RESIN : DESERT_RESIN;
+        BlockState sticky_resin = this.jungle ? STICKY_JUNGLE_RESIN : STICKY_DESERT_RESIN;
+        this.generateSphereRespectAir(world, rand, up, size + 4, height + 2, resin, sticky_resin);
+        this.generateSphere(world, rand, up.above(), size, height, Blocks.AIR.defaultBlockState());
+        this.decorateSphere(world, rand, up.above(), size, height - 1, WorldGenMyrmexHive.RoomType.ENTERANCE);
+        this.hive.getEntrances().put(up, direction);
+        ++this.entrances;
+    }
+
+
+    @Overwrite
+    private void generateCircleRespectSky(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, Direction direction) {
+        BlockState resin = this.jungle ? JUNGLE_RESIN : DESERT_RESIN;
+        BlockState sticky_resin = this.jungle ? STICKY_JUNGLE_RESIN : STICKY_DESERT_RESIN;
+        int radius = size + 2;
+
+        float i;
+        float j;
+        int x;
+        int z;
+        for (i = 0.0F; i < (float) radius; i = (float) ((double) i + 0.5)) {
+            for (j = 0.0F; (double) j < 9.583185307179586 * (double) i; j = (float) ((double) j + 0.5)) {
+                x = (int) Math.floor((double) (Mth.sin(j) * i));
+                z = (int) Math.floor((double) (Mth.cos(j) * i));
+                if (direction != Direction.WEST && direction != Direction.EAST) {
+                    if (!world.canSeeSkyFromBelowWater(position.offset(x, z, 0))) {
+                        world.setBlock(position.offset(x, z, 0), rand.nextInt(3) == 0 ? sticky_resin : resin, 3);
+                    }
+                } else if (!world.canSeeSkyFromBelowWater(position.offset(0, x, z))) {
+                    world.setBlock(position.offset(0, x, z), rand.nextInt(3) == 0 ? sticky_resin : resin, 3);
+                }
+            }
+        }
+    }
+
+    @Shadow
+    abstract void generateSphereRespectAir(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, BlockState fill, BlockState fill2);
 
     @Shadow
     abstract void decorate(LevelAccessor world, BlockPos blockpos, BlockPos center, int size, RandomSource random, WorldGenMyrmexHive.RoomType roomType);
@@ -110,6 +240,15 @@ public abstract class WorldGenMyrmexHiveMixin extends Feature<NoneFeatureConfigu
     abstract void decorateTubers(LevelAccessor world, BlockPos blockpos, RandomSource random, WorldGenMyrmexHive.RoomType roomType);
 
     @Shadow
-    abstract void generateMainRoom(ServerLevelAccessor world, RandomSource rand, BlockPos position);
+    abstract void generatePath(LevelAccessor world, RandomSource rand, BlockPos offset, int length, Direction direction, int roomChance);
+
+    @Shadow
+    abstract void generateSphere(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, BlockState fill);
+
+    @Shadow
+    abstract void generateSphere(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, BlockState fill, BlockState fill2);
+
+    @Shadow
+    abstract void decorateSphere(LevelAccessor world, RandomSource rand, BlockPos position, int size, int height, WorldGenMyrmexHive.RoomType roomType);
 
 }
